b45a340c54c07f5b9df827c283733ed2689dd7b0
GoGradually <chkun3109@gmail.com>
2025-05-29 18:48:02 +0900

    docs: 설명 수정

--- src/main/java/com/example/nplusone/practice/repository/ParentRepository.java
+++ src/main/java/com/example/nplusone/practice/repository/ParentRepository.java
@@ -16,10 +16,8 @@ public interface ParentRepository extends JpaRepository<Parent, Long> {
      * --------------
      * select p from Parent p join fetch p.sonList s join fetch p.grandParent gp
      * N+1 문제는 연관관계를 기준으로, N개를 1번에 조회했다가 N개에서 다시 m개(1~x)를 조회하는데 1번의 쿼리를 더 써서, 1+N번 쿼리를 날리는걸 의미한다.
-     * - 만약 Many To One 관계라면, 1차캐시에 이미 조회되어 있을테니, 쿼리가 한번만 더 나가는거고,
-     * - One To Many 관계라면, 1차캐시에 영속상태로 존재하지 않으므로, 각각에 대해서 모두 쿼리가 날아간다.
-     * - 따라서 Many To One 관계라면, 쿼리가 1+1회가 되고
-     * - One To Many 관계라면, 쿼리가 1+N회가 된다.
+     * - ManyToOne, OneToMany에 관계없이, 조회하려는 대상이 영속성 컨텍스트에 영속상태로 존재하면 해당 객체를 사용하고
+     * - 조회하려는 대상이 영속성 컨텍스트에 존재하지 않으면, 다시 DB에 SQL을 보낸다.
      * --------------
      * select p from Parent p join fetch p.sonList s join fetch p.grandParent gp join fetch p.daughterList d
      * Multibag Exception이 발생한다.
